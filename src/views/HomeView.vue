f82a60822546dd34e64e2b1b342d454385ba2323
Ricardo Aguirre <ra.lopez@devsalsa.com>
2022-07-04 15:51:04 -0500

    Feat: Add portfolio list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import items from "@/assets/data/portfolio.json";
 </script>
 
 <template>
   <main class="container">
     <div class="grid grid-cols-4 gap-4">
-      
+      <div class="drop-shadow-lg p-2" v-for="(item, key) in items" :key="key">
+        <img
+          :src="`/src/assets/img/portfolio/${item.img}`"
+          alt=""
+          class="relative z-10 object-cover h-48 w-full rounded overflow-hidden"
+        />
+      </div>
     </div>
   </main>
 </template>
