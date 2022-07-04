333646d3115ee04591fed67066fb43c714336a4f
AIR <air@192.168.1.10>
2022-07-04 08:41:35 -0500

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class="container">
+    <div class="grid grid-cols-4 gap-4">
+      
+    </div>
+  </main>
+</template>
