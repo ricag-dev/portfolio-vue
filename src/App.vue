cdea75fc5257db82c08362df7263f91b0dd38235
Ricardo Aguirre <ra.lopez@devsalsa.com>
2022-07-04 18:30:34 -0500

    Feat: making code style whit tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="h-screen">
-    
+  <header class="h-screen flex items-center text-5xl justify-center">
+    <div class="container">
+      <div class="w-2/3 m-auto">
+        WELCOME to my profile develop
+        <img src="" />
+      </div>
+    </div>
   </header>
-
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,46 @@ import items from "@/assets/data/portfolio.json";
 </script>
 
 <template>
-  <main class="container">
-    <div class="grid grid-cols-4 gap-4">
-      <div class="drop-shadow-lg p-2" v-for="(item, key) in items" :key="key">
+  <main class="container m-auto">
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div
+        class="
+          rounded
+          overflow-hidden
+          shadow
+          relative
+          hover:shadow-lg hover:scale-125
+          transition-all
+        "
+        v-for="(item, key) in items"
+        :key="key"
+      >
         <img
           :src="`/src/assets/img/portfolio/${item.img}`"
           alt=""
-          class="relative z-10 object-cover h-48 w-full rounded overflow-hidden"
+          class="relative object-cover h-48 w-full"
         />
+        <div
+          class="
+            absolute
+            inset-0
+            backdrop-blur-md backdrop-brightness-150
+            md:backdrop-filter-none
+            bg-white bg-opacity-40
+            text-white
+            flex
+            items-end
+            p-10
+          "
+        >
+          <span
+            :key="key"
+            v-for="(skill, key) in item.tecnologies"
+            class="mr-2"
+          >
+            {{ skill }}
+          </span>
+        </div>
       </div>
     </div>
   </main>
